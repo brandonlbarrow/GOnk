5ee4da0753eb64fc9821b6efb3456f8ad190138e
brandonlbarrow <brandon.barrow101@gmail.com>
2021-11-11 00:02:03 -0600

    first pass at a state machine

---
+++ internal/state/machine.go
@@ -0,0 +1,44 @@
+package state
+
+import (
+	"errors"
+	"sync"
+)
+
+type State string
+
+type StateMachine struct {
+	states map[State]Event
+	lock   *sync.Mutex
+}
+
+type Event func(state State) State
+
+func NewStateMachine() *StateMachine {
+	sm := make(map[State]Event)
+	return &StateMachine{states: sm}
+}
+
+type StateEvent struct {
+	State
+	Event
+}
+
+func (s *StateMachine) RegisterState(stateEvents ...StateEvent) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	for _, stateEvent := range stateEvents {
+		if stateEvent.State != "" && stateEvent.Event != nil {
+			s.states[stateEvent.State] = stateEvent.Event
+		}
+	}
+}
+
+func (s *StateMachine) ProcessState(state State) (State, error) {
+	event, ok := s.states[state]
+	if !ok {
+		return "", errors.New("provided state not valid")
+	}
+	next := event(state)
+	return next, nil
+}
