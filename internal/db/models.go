1a9bed2b56a8da3b7ed2c557d9b1c651e516302c
Brandon Barrow <brandon.barrow101@gmail.com>
2023-05-19 20:30:33 -0500

    start on some data structures

---
+++ internal/db/models.go
@@ -0,0 +1,17 @@
+package db
+
+// Server is a Discord server
+type Server struct {
+	// GuildID is the ID of the Discord server
+	GuildID   string      `json:"guild_id"`
+	Streamers []*Streamer `json:"streamers"`
+}
+
+// ServerMap is a datastore mapping GuildIDs (Server IDs) to Server configurations
+type ServerMap map[string]*Server
+
+// Streamer is a Discord user and channel combination to post stream updates for when receiving Twitch stream online notification events.
+type Streamer struct {
+	UserID    string `json:"user_id"`
+	ChannelID string `json:"channel_id"`
+}
